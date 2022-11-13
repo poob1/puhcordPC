3a393cf95680eb6927afafe4fd4693d315cd911c
poob1 <ddnoidenis@gmail.com>
2022-11-13 12:12:11 +0200

    for real now

--- src/plugins/settings.tsx
+++ src/plugins/settings.tsx
@@ -64,7 +64,7 @@ export default definePlugin({
                 : "";
         return (
             <>
-                <Component {...props}>puhcordPC{gitHash}{additionalInfo}</Component>
+                <Component {...props}>puhcordPC {gitHash}{additionalInfo}</Component>
                 <Component {...props}>da electric {VencordNative.getVersions().electron}</Component>
                 <Component {...props}>da chromium {VencordNative.getVersions().chrome}</Component>
             </>
